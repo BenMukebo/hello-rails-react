624e39a0446c0164dba154bad08ba0befdf00c93
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-26 11:44:04 +0200

    Create home component

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Home = ({ text }) => (
+//   <>
+//     Text:
+//     {' '}
+//     {text}
+//   </>
+// );
+
+const Home = () => (
+  <h1>welcome home!!</h1>
+);
+
+export default Home;
